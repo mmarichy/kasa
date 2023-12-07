5b705a0fceeeda14f5265f3c0603aa1ccd39e932
mathis marichy <marichy.pro@gmail.com>
2023-12-07 16:37:41 +0100

    Add React Router | Deleting unused file

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,9 @@
+function About() {
+  return (
+    <div>
+      <h1>A-propos de nous 👩‍💻👨‍💻👩‍💻</h1>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h1> Bonjour 🏡</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App
